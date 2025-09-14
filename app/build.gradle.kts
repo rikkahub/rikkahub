932d20a5dbd6a1ff677968ef29ea16e2e783f8e9
RE <qq1609403959@outlook.com>
2025-09-14 22:01:40 +0800

    chore: bump version to 1.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 102
-        versionName = "1.6.3"
+        versionCode = 103
+        versionName = "1.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
