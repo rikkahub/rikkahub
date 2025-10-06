df9529b2efbf365147ac148b251445b606a7950a
RE <qq1609403959@outlook.com>
2025-10-06 15:20:56 +0800

    chore: bump app version to 1.6.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 107
-        versionName = "1.6.8"
+        versionCode = 108
+        versionName = "1.6.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
