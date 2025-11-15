158e59deaf0cc7de716e6e455e3650f1e2712be5
RE <qq1609403959@outlook.com>
2025-11-15 23:02:06 +0800

    chore: 改进侧边栏

--- app/src/main/java/me/rerere/rikkahub/ui/pages/chat/ChatDrawer.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/chat/ChatDrawer.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -32,8 +33,10 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.composables.icons.lucide.Bot
 import com.composables.icons.lucide.Lucide
 import com.composables.icons.lucide.Pencil
+import com.composables.icons.lucide.Settings
 import com.composables.icons.lucide.Settings2
 import com.composables.icons.lucide.Sparkles
 import kotlinx.coroutines.flow.first
@@ -45,6 +48,7 @@ import me.rerere.rikkahub.data.model.Conversation
 import me.rerere.rikkahub.data.repository.ConversationRepository
 import me.rerere.rikkahub.ui.components.ai.AssistantPicker
 import me.rerere.rikkahub.ui.components.ui.Greeting
+import me.rerere.rikkahub.ui.components.ui.Tooltip
 import me.rerere.rikkahub.ui.components.ui.UIAvatar
 import me.rerere.rikkahub.ui.components.ui.UpdateCard
 import me.rerere.rikkahub.ui.hooks.EditStateContent
@@ -52,6 +56,7 @@ import me.rerere.rikkahub.ui.hooks.readBooleanPreference
 import me.rerere.rikkahub.ui.hooks.rememberIsPlayStoreVersion
 import me.rerere.rikkahub.ui.hooks.useEditState
 import me.rerere.rikkahub.ui.modifier.onClick
+import me.rerere.rikkahub.ui.theme.LocalDarkMode
 import me.rerere.rikkahub.utils.navigateToChatPage
 import me.rerere.rikkahub.utils.toDp
 import org.koin.compose.koinInject
@@ -97,6 +102,56 @@ fun ChatDrawerContent(
             if (settings.displaySetting.showUpdates && !isPlayStore) {
                 UpdateCard(vm)
             }
+
+            ConversationList(
+                current = current,
+                conversations = conversations,
+                conversationJobs = conversationJobs.keys,
+                searchQuery = searchQuery,
+                onSearchQueryChange = { vm.updateSearchQuery(it) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                onClick = {
+                    navigateToChatPage(navController, it.id)
+                },
+                onRegenerateTitle = {
+                    vm.generateTitle(it, true)
+                },
+                onDelete = {
+                    vm.deleteConversation(it)
+                    if (it.id == current.id) {
+                        navigateToChatPage(navController)
+                    }
+                },
+                onPin = {
+                    vm.updatePinnedStatus(it)
+                }
+            )
+
+            // 助手选择器
+            AssistantPicker(
+                settings = settings,
+                onUpdateSettings = {
+                    vm.updateSettings(it)
+                    scope.launch {
+                        val id = if (context.readBooleanPreference("create_new_conversation_on_start", true)) {
+                            Uuid.random()
+                        } else {
+                            repo.getConversationsOfAssistant(it.assistantId)
+                                .first()
+                                .firstOrNull()
+                                ?.id ?: Uuid.random()
+                        }
+                        navigateToChatPage(navController = navController, chatId = id)
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                onClickSetting = {
+                    val currentAssistantId = settings.assistantId
+                    navController.navigate(Screen.AssistantDetail(id = currentAssistantId.toString()))
+                }
+            )
 
             // 用户头像和昵称自定义区域
             Row(
@@ -155,61 +210,23 @@ fun ChatDrawerContent(
                 }
             }
 
-            ConversationList(
-                current = current,
-                conversations = conversations,
-                conversationJobs = conversationJobs.keys,
-                searchQuery = searchQuery,
-                onSearchQueryChange = { vm.updateSearchQuery(it) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f),
-                onClick = {
-                    navigateToChatPage(navController, it.id)
-                },
-                onRegenerateTitle = {
-                    vm.generateTitle(it, true)
-                },
-                onDelete = {
-                    vm.deleteConversation(it)
-                    if (it.id == current.id) {
-                        navigateToChatPage(navController)
-                    }
-                },
-                onPin = {
-                    vm.updatePinnedStatus(it)
-                }
-            )
-
-            // 助手选择器
-            AssistantPicker(
-                settings = settings,
-                onUpdateSettings = {
-                    vm.updateSettings(it)
-                    scope.launch {
-                        val id = if (context.readBooleanPreference("create_new_conversation_on_start", true)) {
-                            Uuid.random()
-                        } else {
-                            repo.getConversationsOfAssistant(it.assistantId)
-                                .first()
-                                .firstOrNull()
-                                ?.id ?: Uuid.random()
-                        }
-                        navigateToChatPage(navController = navController, chatId = id)
-                    }
-                },
-                modifier = Modifier.fillMaxWidth(),
-                onClickSetting = {
-                    val currentAssistantId = settings.assistantId
-                    navController.navigate(Screen.AssistantDetail(id = currentAssistantId.toString()))
-                }
-            )
-
             Row(
-                modifier = Modifier.padding(horizontal = 12.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
             ) {
+                DrawerAction(
+                    icon = {
+                        Icon(imageVector = Lucide.Bot, contentDescription = stringResource(R.string.assistant_page_title))
+                    },
+                    label = {
+                        Text(stringResource(R.string.assistant_page_title))
+                    },
+                    onClick = {
+                        navController.navigate(Screen.Assistant)
+                    },
+                )
+
                 DrawerAction(
                     icon = {
                         Icon(Lucide.Sparkles, "Menu")
@@ -220,18 +237,18 @@ fun ChatDrawerContent(
                     onClick = {
                         navController.navigate(Screen.Menu)
                     },
-                    modifier = Modifier.weight(1f)
                 )
+
+                Spacer(Modifier.weight(1f))
 
                 DrawerAction(
                     icon = {
-                        Icon(Lucide.Settings2, null)
+                        Icon(Lucide.Settings, null)
                     },
                     label = { Text(stringResource(R.string.settings)) },
                     onClick = {
                         navController.navigate(Screen.Setting)
                     },
-                    modifier = Modifier.weight(1f)
                 )
             }
         }
@@ -287,23 +304,22 @@ private fun DrawerAction(
     Surface(
         onClick = onClick,
         modifier = modifier,
-        color = Color.Transparent,
+        color = MaterialTheme.colorScheme.primaryContainer,
         shape = CircleShape,
-        contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        contentColor = MaterialTheme.colorScheme.onSurface,
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround,
-            modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 4.dp)
-                .fillMaxWidth()
+        Tooltip(
+            tooltip = {
+               label()
+            }
         ) {
             Box(
-                modifier = Modifier.size(24.dp),
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(20.dp),
             ) {
                 icon()
             }
-            label()
         }
     }
 }
