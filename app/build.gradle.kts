34d755e5c5a104a4da5e4f71f75530ba4672e53e
re-ovo <1609403959@qq.com>
2026-04-26 17:36:00 +0800

    build(android): 更新应用版本号至 2.1.11
    - 将 versionCode 从 153 更新到 154
    - 将 versionName 从 "2.1.10" 更新到 "2.1.11"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 153
-        versionName = "2.1.10"
+        versionCode = 154
+        versionName = "2.1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
