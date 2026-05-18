0c9c418124ed799a6661ded5ed86c49de869da1e
re-ovo <1609403959@qq.com>
2026-05-18 16:21:14 +0800

    build: 新增workspace模块和proot lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -292,6 +292,7 @@ dependencies {
     implementation(project(":speech"))
     implementation(project(":common"))
     implementation(project(":material3"))
+    implementation(project(":workspace"))
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
     implementation(kotlin("reflect"))
 
