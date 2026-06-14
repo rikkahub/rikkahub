574043bb205f288ee7b808ca830782c9822e0a35
re-ovo <1609403959@qq.com>
2026-06-14 23:22:33 +0800

    chore: 版本号更新至 2.3.0 (163)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 162
-        versionName = "2.2.6"
+        versionCode = 163
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
