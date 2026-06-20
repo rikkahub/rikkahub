a2e2f6369ecd815c5b52d2ed84e7291a0a0496ca
Pheobe-Southwood <hannu666@163.com>
2026-06-20 10:40:30 +0800

    build: limit ABI to arm64-v8a only for release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
-            abiFilters += listOf("arm64-v8a", "x86_64")
+            abiFilters += listOf("arm64-v8a")
         }
     }
 
@@ -39,7 +39,7 @@ android {
             val isBuildingBundle = gradle.startParameter.taskNames.any { it.lowercase().contains("bundle") }
             isEnable = !isBuildingBundle
             reset()
-            include("arm64-v8a", "x86_64")
+            include("arm64-v8a")
             isUniversalApk = true
         }
     }
