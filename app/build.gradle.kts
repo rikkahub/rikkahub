a383c209bf5429383188fdd0ca09da9465a552de
re-ovo <1609403959@qq.com>
2026-06-28 01:42:28 +0800

    chore(release): 升级版本至 2.3.3 (166)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 165
-        versionName = "2.3.2"
+        versionCode = 166
+        versionName = "2.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
