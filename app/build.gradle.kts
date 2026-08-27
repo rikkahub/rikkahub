da5fd77bcd4f04a2aab77deeed8be85466f79ada
re-ovo <1609403959@qq.com>
2026-08-27 15:50:07 +0800

    chore: bump to 2.4.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 180
-        versionName = "2.4.13"
+        versionCode = 181
+        versionName = "2.4.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
